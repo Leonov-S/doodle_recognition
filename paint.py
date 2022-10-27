54488dda36dd793b6390e489bf721d161bb55834
STARBOY <julien.espes@epitech.eu>
2022-10-27 17:07:03 +0200

    run time recognition + optimization

--- paint.py
+++ paint.py
@@ -1,26 +1,25 @@
 from tkinter import *
-import PIL
 from PIL import Image, ImageDraw
 import cv2
 import numpy as np
 import tensorflow as ts
-import time
-
 from doodle_recognition import *
 
-def save(model):
-    global image_number
-    filename = f'image_0.png'
-    image1.save(filename)
-    img = cv2.imread('./image_0.png', cv2.IMREAD_UNCHANGED)
-    resized = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA)
-    resized = (255-resized)
-    cv2.imwrite("rescaled.png", resized)
-    time.sleep(1)
-    im = Image.open('rescaled.png')
-    numpy_data = np.asarray(im.convert('1'))
-    recognize_img(numpy_data, model)
 
+def clear():
+    cv.delete('all')
+    draw.rectangle((0, 0, 500, 500), fill='white')
+
+def save(e):
+    global loading
+    if (loading == True): return
+    loading = True
+    pix = np.array(image1)
+    resized = cv2.resize(pix, (28, 28), interpolation = cv2.INTER_AREA)
+    resized = (255-resized)
+    guess = recognize_img(resized, model)
+    text.set(guess)
+    loading = False
 
 def activate_paint(e):
     global lastx, lasty
@@ -30,22 +29,28 @@ def activate_paint(e):
 def paint(e):
     global lastx, lasty
     x, y = e.x, e.y
-    cv.create_oval(lastx, lasty, x, y, fill='black', width=10)
-    draw.line((lastx, lasty, x, y), fill='black', width=12)
+    cv.create_line((lastx, lasty, x, y), fill='black', width=20, capstyle=ROUND, smooth=TRUE, splinesteps=36)
+    draw.line((lastx, lasty, x, y), fill='black', width=50)
     lastx, lasty = x, y
 
 model = ts.keras.models.load_model('./dr.h5')
 model.load_weights('./drWeight.h5')
+loading = False
 root = Tk()
 lastx, lasty = None, None
-cv = Canvas(root, width=280, height=280, bg='white')
-image1 = PIL.Image.new('RGB', (280, 280), 'white')
+cv = Canvas(root, width=840, height=840, bg='white')
+image1 = Image.new('L', (840, 840), 'white')
 draw = ImageDraw.Draw(image1)
+text = StringVar()
+text.set("Draw!")
 
 cv.bind('<1>', activate_paint)
+cv.bind('<ButtonRelease-1>', save)
 cv.pack(expand=YES, fill=BOTH)
 
-btn_save = Button(text="save", command= lambda: save(model))
-btn_save.pack()
+text_label = Label(root, textvariable = text, bg = 'white', fg = 'black', font = ('Helvetica', 40))
+btn_clear = Button(text="clear", command=clear)
+btn_clear.pack()
+text_label.pack()
 
 root.mainloop()
